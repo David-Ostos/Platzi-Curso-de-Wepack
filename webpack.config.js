60dcd93b69aa9411b1289ae06158cd16569f3c4f
David <davidostos2@gmail.com>
2023-03-30 13:01:16 -0400

    clase 05-10 css, copia de archivos,loadres de imagenes, loaders de fuentes, optimizacion de hashes, alias

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,28 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',                          // es la direccion donde se vas an a ejecutar el js, que es lo que prepara webpack
+  entry: './src/index.js',                                         // es la direccion donde se vas an a ejecutar el js, que es lo que prepara webpack
   output: {
-    path: path.resolve(__dirname, 'dist'),         // se utiliza path para poder utilizar resove que funciona para saber donde se encuentra nuestro proyecto en el directorio.
-    filename: 'main.js',                           /* aqui se le coloca el nombre del resultante de del enpaquetado */
-    clean: true                                   // se utiliza para qeu limpie automaticamente la carpeta dist dist
+    path: path.resolve(__dirname, 'dist'),                      // se utiliza path para poder utilizar resove que funciona para saber donde se encuentra nuestro proyecto en el directorio.
+    filename: '[name].[contenthash].js',                                      /* aqui se le coloca el nombre del resultante de del enpaquetado */
+    assetModuleFilename: 'assets/images/[hash][ext][query]',
+    clean: true                                              // se utiliza para qeu limpie automaticamente la carpeta dist dist
   },
   resolve: {
-    extensions: ['.js']                            /* para poder identificar los archivos que tiene que seguir */
+    extensions: ['.js'],                                      /* para poder identificar los archivos que tiene que seguir */
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts/')
+    }
   },
   module: {
     rules: [
@@ -21,6 +33,24 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css|.scss$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name].[contenthash][ext]'
+        }
       }
     ]
   },
@@ -29,6 +59,24 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images'
+        }
+      ]
     })
-  ]
+  ],
+  optimization: {                             // aqui se crea la estructura para la automatizacion
+    minimize: true,                          // se le indica que si se va a optimizar
+    minimizer: [                             // se le identifica que metodos se van a utilizar
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
 };
