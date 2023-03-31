4e79ee10378925caa01451831b7f90468c2e8480
David <davidostos2@gmail.com>
2023-03-31 15:20:26 -0400

    clase 14 watch y deploy en netlify

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -14,6 +14,7 @@ module.exports = {
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   mode: 'development',
+  // watch: true,                  /* para activar el modo watch indefinidamente */
   resolve: {
     extensions: ['.js'],
     alias: {
